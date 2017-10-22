141a96cbdd7a27751c52469d7e9f124fa6cc8e0c
Robert Beckson <becksonq@gmail.com>
2017-10-22 14:19:04 +0300

    'update_role'

--- views/access/updateRole.php
+++ views/access/updateRole.php
@@ -5,60 +5,60 @@ use Yii;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
-/** @var  $role  */
-/** @var  $role_permit  */
-/** @var  $permissions  */
+/** @var  $role */
+/** @var  $role_permit */
+/** @var  $permissions */
 
-$this->title = Yii::t('db_rbac', 'Редактирование роли: ') . ' ' . $role->name;
+$this->title = Yii::t( 'db_rbac', 'Редактирование роли: ' ) . ' ' . $role->name;
 
 $this->params['breadcrumbs'][] = [ 'label' => 'Users', 'url' => '/permit/user/index' ];
-$this->params['breadcrumbs'][] = ['label' => Yii::t('db_rbac', 'Управление ролями'), 'url' => ['role']];
-$this->params['breadcrumbs'][] = Yii::t('db_rbac', 'Редактирование');
+$this->params['breadcrumbs'][] = [ 'label' => Yii::t( 'db_rbac', 'Управление ролями' ), 'url' => [ 'role' ] ];
+$this->params['breadcrumbs'][] = Yii::t( 'db_rbac', 'Редактирование' );
 ?>
 <div class="news-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+	<h1><?= Html::encode( $this->title ) ?></h1>
 
-    <div class="links-form">
+	<div class="links-form">
 
-        <?php
-        if (!empty($error)) {
+      <?php
+      if ( !empty( $error ) ) {
+          ?>
+				<div class="error-summary">
+            <?php
+            echo implode( '<br>', $error );
             ?>
-            <div class="error-summary">
-                <?php
-                echo implode('<br>', $error);
-                ?>
-            </div>
-        <?php
-        }
-        ?>
+				</div>
+          <?php
+      }
+      ?>
 
-        <?php $form = ActiveForm::begin([
-		 'options' => [
-		 	'class' => 'form'
-		 ]
-		]); ?>
+      <?php $form = ActiveForm::begin( [
+          'options' => [
+              'class' => 'form'
+          ]
+      ] ); ?>
 
-        <div class="form-group">
-            <?= Html::label(Yii::t('db_rbac', 'Название роли')); ?>
-            <?= Html::textInput('name', $role->name, [ 'class' => 'form-control' ] ); ?>
-        </div>
+		<div class="form-group">
+        <?= Html::label( Yii::t( 'db_rbac', 'Название роли' ) ); ?>
+        <?= Html::textInput( 'name', $role->name, [ 'class' => 'form-control' ] ); ?>
+		</div>
 
-        <div class="form-group">
-            <?= Html::label(Yii::t('db_rbac', 'Текстовое описание')); ?>
-            <?= Html::textInput('description', $role->description, [ 'class' => 'form-control' ] ); ?>
-        </div>
+		<div class="form-group">
+        <?= Html::label( Yii::t( 'db_rbac', 'Текстовое описание' ) ); ?>
+        <?= Html::textInput( 'description', $role->description, [ 'class' => 'form-control' ] ); ?>
+		</div>
 
-        <div class="form-group">
-            <?= Html::label(Yii::t('db_rbac', 'Разрешенные доступы')); ?>
-            <?= Html::checkboxList('permissions', $role_permit, $permissions, ['separator' => '<br>']); ?>
-        </div>
+		<div class="form-group">
+        <?= Html::label( Yii::t( 'db_rbac', 'Разрешенные доступы' ) ); ?>
+        <?= Html::checkboxList( 'permissions', $role_permit, $permissions, [ 'separator' => '<br>' ] ); ?>
+		</div>
 
-        <div class="form-group">
-            <?= Html::submitButton(Yii::t('db_rbac', 'Сохранить'), ['class' => 'btn btn-success']) ?>
-        </div>
+		<div class="form-group">
+        <?= Html::submitButton( Yii::t( 'db_rbac', 'Сохранить' ), [ 'class' => 'btn btn-success' ] ) ?>
+		</div>
 
-        <?php ActiveForm::end(); ?>
+      <?php ActiveForm::end(); ?>
 
-    </div>
+	</div>
 </div>
